f26ba4c9b7ad458fda7cdb7acdcb0232bf6183fc
t.joao12@gmail.com <t.joao12@gmail.com>
2019-01-15 16:42:58 +0000

    Updates

--- BottleSong.scala
+++ BottleSong.scala
@@ -1,17 +1,18 @@
-object TuesdayTasks extends App {
+object BottleSong extends App {
 
   def task1(bottle1: Int) {
     var bottle2 = bottle1 - 1
     if (bottle1 > 0) {
       println(s"$bottle1 bottles of beer on the wall, $bottle1 bottles of beer.")
       println(s"Take one down and pass it around, $bottle2 bottles of beer on the wall.")
-      task1(bottle1-1)
+      task1(bottle1 - 1)
     }
     else {
       println("No more bottles of beer on the wall, no more bottles of beer.")
       println("Go to the store and buy some more, 99 bottles of beer on the wall.")
     }
   }
-  task1(99)
 
+  task1(99)
+}
  
