29bb154a8caee6e4ff3c75241d7c9df9216b7ccd
Joao Teixeira <45290566+tjoao12@users.noreply.github.com>
2019-03-28 22:05:48 +0000

    Update Cypher.scala

--- Cypher.scala
+++ Cypher.scala
@@ -1,39 +1,46 @@
 object Cypher extends App {
 
-  var list = List[String](".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..")
-  var list2 = ('a' to 'z').toList
+
   var storage = " "
 
 
-  startMenu(storage:String, list: List[String], list2:List[Char])
+  def Cypher(word:String) {
 
-  def startMenu(storage:String, list: List[String], list2:List[Char]) {
-    println("This is the Cypher, press (1) to type a sentence or anything else to quit")
-    scala.io.StdIn.readInt match {
-      case 1 => typeWord(storage:String, list: List[String], list2:List[Char])
-      case _ => sys.exit(0)
+    word match {
+      case "a" | "A" => print("Z")
+      case "b" | "B" => print("Y")
+      case "c" | "C" => print("X")
+      case "d" | "D" => print("W")
+      case "e" | "E" => print("V")
+      case "f" | "F" => print("U")
+      case "g" | "G" => print("T")
+      case "h" | "H" => print("S")
+      case "i" | "I" => print("R")
+      case "j" | "J" => print("Q")
+      case "k" | "K" => print("P")
+      case "l" | "L" => print("O")
+      case "m" | "M" => print("N")
+      case "n" | "N" => print("M")
+      case "o" | "O" => print("L")
+      case "p" | "P" => print("K")
+      case "q" | "Q" => print("J")
+      case "r" | "R" => print("I")
+      case "s" | "S" => print("H")
+      case "t" | "T" => print("G")
+      case "u" | "U" => print("F")
+      case "v" | "V" => print("E")
+      case "w" | "W" => print("D")
+      case "x" | "X" => print("C")
+      case "y" | "Y" => print("B")
+      case "z" | "Z" => print("A")
     }
   }
-  def typeWord (storage:String, list: List[String], list2:List[Char]) {
 
-    println("type in your sentence")
-    var input = scala.io.StdIn.readLine
-    converter(input, storage, list, list2)
-  }
+    def wordSplit (wordCount:String){
 
-  def converter(input: String, storage: String, list: List[String], list2:List[Char]) {
-    var storage = " "
-    for (x <- 0 until input.length) {
-      for (i <- 0 until list2.length) {
-        if (input.substring(x,x+1) == list2(i).toString) {
-          storage+= list(i) + " "
-        }
-      }
-      if (x>0 && storage.substring(x-1,x)!= " " && input.substring(x,x+1)==" ") {
-        storage+= " / "
+      for (x<- 0 until wordCount.length){
+        storage += Cypher(wordCount.substring(x,x+1))
       }
     }
-    println("The outcome is" + storage)
-    startMenu(storage:String, list: List[String], list2:List[Char])
-  }
+    wordSplit("abc")
 }
